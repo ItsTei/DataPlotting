29c735e23013b1bf01cce1f7a0291f6bc503f08d
ItsTei <166406623+ItsTei@users.noreply.github.com>
2026-02-23 10:40:57 +0300

    Create Plot2.R

---
+++ Plot2.R
@@ -0,0 +1,35 @@
+# plot2.R
+# Line plot of Global Active Power over time (2007-02-01 and 2007-02-02)
+
+file_path <- "household_power_consumption.txt"
+
+power <- read.table(
+  file_path,
+  header = TRUE,
+  sep = ";",
+  stringsAsFactors = FALSE,
+  na.strings = "?"
+)
+
+power_sub <- subset(power, Date == "1/2/2007" | Date == "2/2/2007")
+
+# Create datetime
+dt <- strptime(
+  paste(power_sub$Date, power_sub$Time),
+  format = "%d/%m/%Y %H:%M:%S"
+)
+power_sub$Datetime <- as.POSIXct(dt)
+
+power_sub$Global_active_power <- as.numeric(power_sub$Global_active_power)
+
+png("plot2.png", width = 480, height = 480)
+
+plot(
+  power_sub$Datetime,
+  power_sub$Global_active_power,
+  type = "l",
+  xlab = "",
+  ylab = "Global Active Power (kilowatts)"
+)
+
+dev.off()
