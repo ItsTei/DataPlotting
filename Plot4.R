1fe2a0ae5e0272bbd7e73ca457bfdd8b94f8fd78
ItsTei <166406623+ItsTei@users.noreply.github.com>
2026-02-23 12:38:41 +0300

    Update Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -1,26 +1,12 @@
 # plot4.R
-# 2x2 panel plot:
-# (1) Global Active Power
-# (2) Voltage
-# (3) Sub metering (1/2/3)
-# (4) Global Reactive Power
 
 file_path <- "household_power_consumption.txt"
 
-power <- read.table(
-  file_path,
-  header = TRUE,
-  sep = ";",
-  stringsAsFactors = FALSE,
-  na.strings = "?"
-)
+power <- read.table(file_path, header = TRUE, sep = ";", stringsAsFactors = FALSE, na.strings = "?")
 
 power_sub <- subset(power, Date == "1/2/2007" | Date == "2/2/2007")
 
-dt <- strptime(
-  paste(power_sub$Date, power_sub$Time),
-  format = "%d/%m/%Y %H:%M:%S"
-)
+dt <- strptime( paste(power_sub$Date, power_sub$Time), format = "%d/%m/%Y %H:%M:%S")
 power_sub$Datetime <- as.POSIXct(dt)
 
 # Ensure numeric
@@ -36,49 +22,18 @@ png("plot4.png", width = 480, height = 480)
 par(mfrow = c(2, 2), mar = c(4, 4, 2, 1))
 
 # Top-left: Global Active Power
-plot(
-  power_sub$Datetime,
-  power_sub$Global_active_power,
-  type = "l",
-  xlab = "",
-  ylab = "Global Active Power"
-)
+plot( power_sub$Datetime, power_sub$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power")
 
 # Top-right: Voltage
-plot(
-  power_sub$Datetime,
-  power_sub$Voltage,
-  type = "l",
-  xlab = "datetime",
-  ylab = "Voltage"
-)
+plot( power_sub$Datetime, power_sub$Voltage, type = "l", xlab = "datetime", ylab = "Voltage")
 
 # Bottom-left: Sub metering
-plot(
-  power_sub$Datetime,
-  power_sub$Sub_metering_1,
-  type = "l",
-  xlab = "",
-  ylab = "Energy sub metering"
-)
+plot( power_sub$Datetime, power_sub$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
 lines(power_sub$Datetime, power_sub$Sub_metering_2, col = "red")
 lines(power_sub$Datetime, power_sub$Sub_metering_3, col = "blue")
-legend(
-  "topright",
-  legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
-  col = c("black", "red", "blue"),
-  lty = 1,
-  bty = "n",
-  cex = 0.8
-)
+legend( "topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty = 1, bty = "n", cex = 0.8)
 
 # Bottom-right: Global Reactive Power
-plot(
-  power_sub$Datetime,
-  power_sub$Global_reactive_power,
-  type = "l",
-  xlab = "datetime",
-  ylab = "Global Reactive Power"
-)
+plot( power_sub$Datetime, power_sub$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global Reactive Power")
 
 dev.off()
